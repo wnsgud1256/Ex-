8a24d2c08cdc61e40f26e3d43b2fdb2e46ac8b17
User <User@User-PC>
2018-07-13 14:19:42 +0900

    study

---
+++ bookTest/src/bookTest/BookTest01_1.java
@@ -0,0 +1,18 @@
+package bookTest;
+
+public class BookTest01_1 {
+	public static void main(String[] args) {
+		
+		
+		//문제 2
+		System.out.println("2 + 5 = " + 2 + 5); //문자 2와 문자5를 그냥 붙인거기 때문에 결과값이 25가 나오는것음. 숫자로 보는게 아니고 " "<-가 쳐져있지 않더라도 문자로 보기 때문에 25가 나옴 
+		
+		//문제3
+		System.out.println("12");
+		System.out.println((2*6));
+		System.out.println(12);
+		System.out.println("1 + 2 = " + 1 + 2);
+		System.out.println("1 + 2 = " + "1" + "2" );
+		
+	}
+}
